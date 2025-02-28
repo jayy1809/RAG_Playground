58434d081b8485e36aaa472c327ff9f1845603bf
jayy1809 <jaypanchal1809@gmail.com>
2025-02-28 15:07:23 +0530

    updated settings.py in config and requirements.txt

---
+++ app/config/settings.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PINECONE_API_KEY : str
+    GROQ_API_KEY : str
+    LLAMA_CLOUD_API_KEY : str
+    COHERE_API_KEY : str
+    ZILLIS_API_KEY : str
+    JINA_AI_API_KEY : str
+    UNSTRUCTURED_API_KEY : str
+    GEMINI_API_KEY : str
+    MONGO_URI :str
+    DB_NAME :str
+    PINECONE_CREATE_INDEX_URL : str
+    PINECONE_API_VERSION : str
+    PINECONE_EMBED_URL : str
+    PINECONE_UPSERT_URL : str
+    PINECONE_RERANK_URL : str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
