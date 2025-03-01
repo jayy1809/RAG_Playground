198bf9431cc0ba8656177fcb08a34c7d1c5b1bd2
vaghani04 <vaghanimaunik03@gmail.com>
2025-03-01 15:40:19 +0530

    INFO: DB Helper Added

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.apis.file_upload import router as file_router
+from contextlib import asynccontextmanager
+from app.config.database import db_helper
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await db_helper.connect()
+    db = await db_helper.get_db()
+    yield
+    await db_helper.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(file_router)
 
